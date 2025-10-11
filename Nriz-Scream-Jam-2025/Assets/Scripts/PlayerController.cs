ad692de6bfb7ae55002397d119718112f0740957
Nriz <here.is.nriz@gmail.com>
2025-10-11 21:28:31 +0700

    Player and Camera Movement plus Walls

--- Nriz-Scream-Jam-2025/Assets/Scripts/PlayerController.cs
+++ Nriz-Scream-Jam-2025/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    // public
+
+    // SerializeField
+    [SerializeField] float playerSpeed;
+
+    // private
+    private GameManager gameManager;
+    private Rigidbody2D playerRb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();   
+        playerRb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        if (gameManager.gameRunning)
+        {
+            MovePlayer();
+        }
+    }
+
+    private void MovePlayer()
+    {
+        float verticalInput = Input.GetAxis("Vertical");
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        Vector2 playerMoving = new Vector2(horizontalInput, verticalInput);
+
+        playerRb.velocity = playerMoving * playerSpeed * Time.deltaTime;
     }
 }
