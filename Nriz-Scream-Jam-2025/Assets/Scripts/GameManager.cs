f4fe1fff9a4c742cafa87d5e56881cc38731dc41
Nriz <here.is.nriz@gmail.com>
2025-10-11 10:38:03 +0700

    Add Script to Game Manager & Player

---
+++ Nriz-Scream-Jam-2025/Assets/Scripts/GameManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // public
+    public bool gameRunning = true;
+
+    // SerializeField
+
+    // private
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void GamePaused()
+    {
+        Time.timeScale = 0;
+    }
+
+    public void GameOver()
+    {
+        gameRunning = false;
+    }
+
+    public void GameWinning()
+    {
+        gameRunning = false;
+    }
+}
